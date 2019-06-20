ac6077508d45e342459c7f45a32e75b726cea189
Yusuf Duzgun <yusuf@desk.msb.gov.tr>
2019-06-20 15:05:09 +0300

    update dotfiles

--- dotfilesync.sh
+++ dotfilesync.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-COPYPATH="~/git/dotfiles"
-
-cp -Rvf ~/.bashrc $COPYPATH/bashrc 
-cp -Rvf ~/.dwmstart $COPYPATH/dwmstart 
-cp -Rvf ~/.vimrc $COPYPATH/vimrc 
-cp -Rvf ~/.Xdefaults $COPYPATH/Xdefaults 
-cp -Rvf ~/.xinitrc $COPYPATH/xinitrc
+COPYPATH='~/git/dotfiles'
+Cp -vf ~/.bashrc "$COPYPATH"/bashrc 
+cp -vf ~/.dwmstart $COPYPATH/dwmstart 
+cp -vf ~/.vimrc $COPYPATH/vimrc 
+cp -vf ~/.Xdefaults $COPYPATH/Xdefaults 
+cp -vf ~/.xinitrc $COPYPATH/xinitrc
 
 git add .
 git commit -m "update dotfiles"
