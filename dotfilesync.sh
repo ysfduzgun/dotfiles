1fd06f7d5574f41a6ed930f0380d17975450d463
Yusuf Düzgün <yzduzgun@gmail.com>
2019-07-20 07:54:06 +0300

    no need dotfilesycn

--- dotfilesync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -vuf ~/.bashrc ~/git/dotfiles/bashrc 
-cp -vuf ~/.dwmstart ~/git/dotfiles/dwmstart 
-cp -vuf ~/.vimrc ~/git/dotfiles/vimrc 
-cp -vuf ~/.Xdefaults ~/git/dotfiles/Xdefaults 
-cp -vuf ~/.xinitrc ~/git/dotfiles/xinitrc
